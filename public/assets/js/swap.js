16c5e2c4fff6dacd35c44d3e91bd0c20320fdaff
rqver <hunterynz@gmail.com>
2026-01-06 22:06:19 +1300

    wip: Home page hero

---
+++ public/assets/js/swap.js
@@ -0,0 +1,33 @@
+// JS For the text swap/flip effect inspired by https://landonorris.com/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = document.querySelectorAll(".norris-swap");
+
+  targets.forEach((el) => {
+    const originalText = el.textContent;
+    const hoverText = el.getAttribute("data-swap") || originalText;
+
+    const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
+    const originalChars = Array.from(segmenter.segment(originalText), s => s.segment);
+    const hoverChars = Array.from(segmenter.segment(hoverText), s => s.segment);
+
+    const maxLength = Math.max(originalChars.length, hoverChars.length);
+
+    while (originalChars.length < maxLength) originalChars.push("\u00A0");
+    while (hoverChars.length < maxLength) hoverChars.push("\u00A0");
+
+    el.textContent = "";
+
+    originalChars.forEach((char, index) => {
+      const span = document.createElement("span");
+      span.className = "char";
+      span.setAttribute("data-start", char);
+      span.setAttribute("data-end", hoverChars[index]);
+      span.style.setProperty("--index", index);
+      span.textContent = char;
+      el.appendChild(span);
+    });
+
+    el.classList.remove("invisible");
+  });
+});
