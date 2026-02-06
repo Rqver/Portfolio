40fb9b8568a363854919971e4832b458bbce7332
rqver <hunterynz@gmail.com>
2026-02-06 21:52:47 +1300

    wip: Audio TUI Page

---
+++ routes/projects/audio-tui.ts
@@ -0,0 +1,10 @@
+import {join} from "jsr:@std/path";
+import express from "npm:express";
+
+export default {
+    url: '/projects/audio-tui',
+    type: 'GET',
+    callback: async (_: express.Request, res: express.Response) => {
+        res.sendFile(join(Deno.cwd(), "public/projects/audio-tui.html"))
+    }
+}
